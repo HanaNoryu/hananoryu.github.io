ed06a97cbf5b9707d815d69f8e6115be70858455
Noryu <HanaNoryu@outlook.com>
2022-11-03 16:31:56 +0800

    :pencil: update content

--- bash/backup.sh
+++ bash/backup.sh
@@ -3,6 +3,7 @@ info=$1
 if ["$info" = ""];
 then info=":pencil: update content"
 fi
+set HEXO_ALGOLIA_INDEXING_KEY=847373a4187feef5bc36b9224087edb0
 git add -A
 git commit -m "$info"
 git push origin hexo
